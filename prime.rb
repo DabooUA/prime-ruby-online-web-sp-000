8cff13f53242b8f20758bdc066390ee1d380ee57
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-27 03:10:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
-# Add  code here!
+=begin def prime?(number)
+  i = 2
+  while i < number
+
+    if number % i == 0
+      return false
+    end
+
+    i += 1
+
+  end
+
+  return true
+
+end
+=end
+
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test| 
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
